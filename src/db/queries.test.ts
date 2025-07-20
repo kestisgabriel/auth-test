f623c9b17400ad2ab89ce1fa0630fa5abbafdb46
Gabriel <priv@gabrielnyberg.com>
2025-07-20 13:25:31 +0200

    open and close db before and after

--- src/db/queries.test.ts
+++ src/db/queries.test.ts
@@ -1,10 +1,20 @@
-import { describe, expect, it } from 'bun:test'
+import { describe, expect, it, beforeEach, afterEach } from 'bun:test'
 import { createTestDb } from '../test/test-db'
 import { insertUser } from './queries'
+import { Database } from 'bun:sqlite'
+
+let db: Database
+
+beforeEach(() => {
+	db = createTestDb()
+})
+
+afterEach(() => {
+	db.close()
+})
 
 describe('insertUser', () => {
 	it('should insert a user into the database', async () => {
-		const db = createTestDb()
 		const email = 'a@b.com'
 		const password = 'password'
 		const userId = await insertUser(db, email, password)
@@ -13,7 +23,6 @@ describe('insertUser', () => {
 	})
 
 	it('should throw error if email is already in db', async () => {
-		const db = createTestDb()
 		const email = 'a@b.com'
 		const password = 'password'
 		await insertUser(db, email, password)
@@ -25,6 +34,20 @@ describe('insertUser', () => {
 			expect(error).toBeInstanceOf(Error)
 			// @ts-ignore
 			expect(error.message).toMatch(/UNIQUE constraint failed/)
+		}
+	})
+
+	it('should throw error if password is empty', async () => {
+		const email = 'a@b.com'
+		const password = ''
+
+		try {
+			await insertUser(db, email, password)
+		} catch (error) {
+			console.log(error)
+			expect(error).toBeInstanceOf(Error)
+			// @ts-ignore
+			expect(error.message).toMatch(/password must not be empty/)
 		}
 	})
 })
