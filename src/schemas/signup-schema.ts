b0a5555156670f566dec4d313f2ef1b635aa40a0
Gabriel <priv@gabrielnyberg.com>
2025-07-20 13:34:26 +0200

    signup schema

---
+++ src/schemas/signup-schema.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod'
+import { zValidator } from '@hono/zod-validator'
+
+export const signupSchema = z.object({
+	email: z.string().email(),
+	password: z
+		.string()
+		.min(8, { message: 'Password must be at least 8 characters' }),
+})
+
+export const signupValidator = zValidator('json', signupSchema, (result, c) => {
+	if (!result.success) {
+		return c.json({
+			// extract nested error messages from zod
+			errors: result.error.issues.map((issue) => issue.message),
+		})
+	}
+})
